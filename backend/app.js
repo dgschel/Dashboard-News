5fb995d64cb301a2ae8c0c53fdd2c23d26b8dd42
David Göschel <goesc_@hotmail.de>
2020-07-19 19:01:45 +0200

    fixed dist folder

--- backend/app.js
+++ backend/app.js
@@ -15,11 +15,15 @@ app.use(cors())
 app.use('/api/news/', newsRouter)
 app.use('/api/articles/', articleRouter)
 app.use('/api/user/', userRouter)
-
 app.use('/images', express.static(path.join('backend/uploads')))
+
+// create a link to my dist build directory
+const distDir = path.join(__dirname, "../dist");
+app.use(express.static(distDir));
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
+    console.log(distDir)
     console.log(`Server listen on port ${port}`)
 })
